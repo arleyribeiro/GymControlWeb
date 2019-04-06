f2a37bde67bced5a1dd9012b42a7628fdf0cb33e
arleyribeiro <arley.sribeiro@gmail.com>
2019-04-06 17:37:27 -0300

    datime for specific day

--- src/app/grade/grade-form/grade-form.component.ts
+++ src/app/grade/grade-form/grade-form.component.ts
@@ -1,5 +1,5 @@
 import { GradeService } from './../grade.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { fbind } from 'q';
@@ -19,18 +19,20 @@ export class GradeFormComponent implements OnInit {
   courseId = null
   course = null
   daysweek = []
+  selectedsDay = []
   startHour
   endHour
   gradeForm = this.fb.group({
-    name: [''],
-    courseId: [''],
-    startDate: [''],
-    endDate: [''],
-    vacancy: [''],
-    userId: [''],
+    name: ['', Validators.required],
+    courseId: ['', Validators.required],
+    startDate: ['', Validators.required],
+    endDate: ['', Validators.required],
+    vacancy: ['', Validators.required],
+    userId: ['', Validators.required],
     daysweek: ['']
   })
   ngOnInit() {
+    this.initDays();
     if(this.data.courses != null) {
       this.courses = this.data.courses;
       this.course = this.data.course;
@@ -40,24 +42,33 @@ export class GradeFormComponent implements OnInit {
   }
 
   onSubmit(){
-    this.gradeForm.get('daysweek').setValue(this.daysweek);
+    this.validateDaysWeek();
+    this.gradeForm.get('daysweek').setValue(this.selectedsDay);
     this.gradeService.postGrade(this.gradeForm.value).subscribe(response => {
       console.log(response)
     });
     console.log(this.gradeForm.value)
   }
 
-  addDay(day) {
-    this.daysweek.push({
-      startHour: this.startHour,
-      endHour: this.endHour,
-      day: day,
-      gradeId: 0
-    })
+  initDays() {
+    let nameDays = ['Domingo','Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+    for(let i = 0; i < 7; i++)
+      this.daysweek.push({
+        startHour: null,
+        endHour: null,
+        day: i+1,
+        gradeId: 0,
+        name: nameDays[i]
+      })
   }
 
-  test() {
-    console.log(this.startHour)
-    console.log(this.endHour)
+  validateDaysWeek() {
+    console.log("validade day week")
+    this.daysweek.forEach(day =>{
+      console.log(day)
+      if(day.startHour != null && day.endHour != null)
+        this.selectedsDay.push(day);
+    })
+    console.log(this.selectedsDay)
   }
 }
